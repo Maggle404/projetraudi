bb031561d300589a3797e997aa4e63195fe4e845
Berserkyr <144341645+Berserkyr@users.noreply.github.com>
2024-02-01 19:45:23 +0100

    front end

--- controller/carsController.js
+++ controller/carsController.js
@@ -2,7 +2,10 @@ const Car = require("../model/carsModel")
 const sequelize = require("../database/database")
 
 
-exports.catalogue = async(res, req)=>{
-    let result = await Car.findAll()
+exports.catalogue = async(req, res)=>{
+   try { let result = await Car.findAll()
     res.status(200).json(result)
+   } catch (error) {console.error("Erreur lors de la récuperation du catalogue :", error);
+   res.status(500).json({ message: "Erreur lors de la récuperation des données"})
+}
 }
